e1f176bb3e9f0e9462a0a394aed8f3cb82d9f483
lengyeldani <43348697+lengyeldani@users.noreply.github.com>
2021-04-06 18:45:01 +0200

    only non trashed users fixed

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = DB::table('users')->paginate(10);
+        $users = User::where('deleted_at', null)->paginate(10);
         return response($users,200);
     }
 
